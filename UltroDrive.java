fce776278dce0717371388704fee8867dc7dac1b
lbarbaftc <lbarba@browning.edu>
2016-12-07 18:38:49 -0500

    Update UltroDrive.java

--- UltroDrive.java
+++ UltroDrive.java
@@ -1,35 +1,3 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -59,7 +27,7 @@ public class UltroDrive extends OpMode{
 
     /* Declare OpMode members. */
     org.firstinspires.ftc.teamcode.HardwarePushbot robot = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
-                                                         // could also use HardwarePushbotMatrix class.
+    // could also use HardwarePushbotMatrix class.
     double          leftOffset  = 0.0 ;
     double          rightOffset = 0.0;
     // Servo mid position
@@ -100,7 +68,7 @@ public class UltroDrive extends OpMode{
     @Override
     public void loop() {
 
-        float left = -gamepad1.left_stick_y;
+        float left = gamepad1.left_stick_y;
         float right = -gamepad1.right_stick_y;
         //zipties intake
         //float intakeA = -gamepad2.left_stick_y;
@@ -109,7 +77,7 @@ public class UltroDrive extends OpMode{
 
         if (gamepad2.a){
             robot.flyWheelLeftMotor.setPower(-0.2);
-            robot.flyWheelRightMotor.setPower(0.2); 
+            robot.flyWheelRightMotor.setPower(0.2);
         }
         if (gamepad2.b){
             robot.flyWheelLeftMotor.setPower(0);
@@ -138,7 +106,7 @@ public class UltroDrive extends OpMode{
             robot.rightBeacon.setPosition(1.0);
         }
         if (gamepad1.left_bumper){
-            robot.leftBeacon.setPosition(-1.0);
+            robot.leftBeacon.setPosition(0.0);
         }
         if (gamepad1.right_trigger>0.0){
             robot.rightBeacon.setPosition(0.5);
@@ -163,8 +131,8 @@ public class UltroDrive extends OpMode{
         telemetry.addData("rightB", "Offset = %.2f", rightOffset);
         telemetry.addData("left", "left pwr " + String.format("%.2f", left));
         telemetry.addData("right", "right pwr " + String.format("%.2f", right));
-     //   telemetry.addData("flyWheelLeft tgt pwr", "flyWheelLeft pwr " + String.format("%.2f", flyWheelLeft));
-    //    telemetry.addData("flyWheelRight tgt pwr", "flyWheelRight pwr " + String.format("%.2f", flyWheelRight));
+        //   telemetry.addData("flyWheelLeft tgt pwr", "flyWheelLeft pwr " + String.format("%.2f", flyWheelLeft));
+        //    telemetry.addData("flyWheelRight tgt pwr", "flyWheelRight pwr " + String.format("%.2f", flyWheelRight));
         updateTelemetry(telemetry);
     }
 
